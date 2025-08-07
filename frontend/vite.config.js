f0bcbdf26f4c88e33d2e336489b6146c7dd71b8f
Arshit0028 <dhimanarshit99@gmail.com>
2025-08-08 00:00:04 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Update 'base' if you're deploying to GitHub Pages (e.g. /your-repo-name/)
 export default defineConfig({
-  base: '/MediGo',
   plugins: [react()],
-  server: { port: 5173 }
+  server: { port: 5173 },
+  base: './', // <- Important for relative path resolution
 })
